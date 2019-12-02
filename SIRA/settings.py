20dc5d88cd059ed816877115e2eecd6d549a9cc3
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2019-12-02 17:28:00 +0000

    Fixed Host error in the allowed user

--- SIRA/settings.py
+++ SIRA/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 
 if os.path.exists('env.py'):
-    import env.py
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'storages',
 ]
 
