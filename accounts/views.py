706119b54048021849c20c3b38f4940052c2166e
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2019-12-02 17:37:30 +0000

    Added Logout functionality to app

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
 
 
 # View to display the index page
 def index(request):
     return render(request, 'index.html')
+    
+# View to log user out
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('index'))
