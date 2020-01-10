f2ce326ee73ed69b7dc57598b7e1f7fd91151884
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2020-01-10 00:50:56 +0000

    Change the Model for the application, capability to add more images, and removed some incorrect code

--- adoptables/models.py
+++ adoptables/models.py
@@ -5,28 +5,20 @@ from django.urls import reverse
 class Adoptable(models.Model):
     name = models.CharField(max_length=100, default='')
     description = models.TextField()
-    age = models.CharField(max_length=100, default='unknown')
-    breed = models.CharField(max_length=100, default='mix breed')
-    
+    age = models.CharField(max_length=100, default='Unknown')
+    breed = models.CharField(max_length=100, default='Mix breed')
     sex = models.CharField(max_length=100, default='')
-    
-    SMALL = 'Small'
-    MEDIUM = 'Medium'
-    LARGE = 'Large'
-    size =[(SMALL, 'Small'), (MEDIUM, 'Medium'), (LARGE, 'Large')]
-    
-    ADOPTED = 'Adopted'
-    FOSTERED = 'Fostered'
-    SHELTER = 'Shelter'
-    situation = [(ADOPTED, 'Adopted'), (FOSTERED, 'Fostered'), (SHELTER, 'Shelter')]
-    
+    size =models.CharField(max_length=100, default='')
+    situation = models.CharField(max_length=100, default='')
     status = models.BooleanField(verbose_name='For adoption', default=True)
-    date_of_birth = models.CharField(max_length=100, default='unknown')
+    date_of_birth = models.CharField(max_length=100, default='Unknown')
     in_shelter_from = models.CharField(max_length=100, default='')
     location = models.CharField(max_length=100, default='')
     
     dog_image1 = models.ImageField(upload_to='dog_images', blank=True)
     dog_image2 = models.ImageField(upload_to='dog_images', blank=True, null=True)
+    dog_image3 = models.ImageField(upload_to='dog_images', blank=True, null=True)
+    dog_image4 = models.ImageField(upload_to='dog_images', blank=True, null=True)
     
     def get_absolute_url(self):
         return reverse('dog_details', kwargs={'id':self.id})
