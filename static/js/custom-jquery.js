fc7d95df1915ca23ee2854b59bbdbea24ff25592
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2020-01-17 17:35:42 +0000

    Added static files to Github

--- static/js/custom-jquery.js
+++ static/js/custom-jquery.js
@@ -2,3 +2,35 @@
 jQuery(document).ready(function($) {
             $(".num").counterUp({delay:15, time:1000});
         });
+
+// jQuery function for form validator         
+//$(function() {
+    //$("form[name='registration']").validate({
+    // Specify validation rules
+   // rules: {
+    //  name: "required",
+     // email: {
+    //    required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+     //   email: true
+     // },
+     // phone: "required",
+     // subject: "required",
+      //question: "required"
+   // },
+    // Specify validation error messages
+   // messages: {
+   //   name: "Please enter your name",
+   //   email: "Please enter a valid email address",
+   //   phone: "Please enter a valid phone nummber",
+   //   subjet: "Please enter your subject for contacting us",
+   //   question: "Please enter your question"
+  //  },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+   // submitHandler: function(form) {
+  //    form.submit();
+  //  }
+ // });
+//});
