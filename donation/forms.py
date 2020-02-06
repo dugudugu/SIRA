295742db6912170d85568699d71fae36e125b9ec
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2020-02-06 20:12:31 +0000

    Fixed error with stripe messages. Added styling in the forms

--- donation/forms.py
+++ donation/forms.py
@@ -13,8 +13,36 @@ class MakePaymentForm(forms.Form):
     expiry_year = forms.ChoiceField(choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
     
+    class Meta:
+        fields = ['credit_card_number', 'cvv', 'expiry_month', 'expiry_year',]
+       
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['credit_card_number'].widget.attrs['class'] = 'input'
+        self.fields['credit_card_number'].widget.attrs['placeholder'] = 'Fill in card number e.q xxxx xxxx xxxx xxxx'
+        
+        self.fields['cvv'].widget.attrs['class'] = 'input'   
+        self.fields['cvv'].widget.attrs['placeholder'] = 'Fill in the CVV. The CVV can be found at the back of your Card'
+        
+        self.fields['expiry_month'].widget.attrs['class'] = 'input'   
+        self.fields['expiry_year'].widget.attrs['class'] = 'input'
+        
 
 class DonationForm(forms.ModelForm):
     class Meta:
         model = Donation
         fields = ['full_name', 'email', 'donation','country',]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['full_name'].widget.attrs['class'] = 'input'
+        self.fields['full_name'].widget.attrs['placeholder'] = 'Full name e.g. Sam Smith'
+        
+        self.fields['email'].widget.attrs['class'] = 'input'   
+        self.fields['email'].widget.attrs['placeholder'] = 'Email e.g. s.smith@gmail.com'
+        
+        self.fields['country'].widget.attrs['class'] = 'input'   
+        self.fields['country'].widget.attrs['placeholder'] = 'Country e.g. Netherlands'
+        
+        self.fields['donation'].widget.attrs['class'] = 'input'
+        self.fields['donation'].widget.attrs['placeholder'] = 'How much would you like to donate'
