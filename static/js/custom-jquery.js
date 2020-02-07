60659fe8b37285464bc9fed757802489f4624c28
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2020-02-07 16:34:05 +0000

    Added functionality for dropdown with jQuery. Change the color of donation button, and changed the donation button on mobile

--- static/js/custom-jquery.js
+++ static/js/custom-jquery.js
@@ -1,36 +1,23 @@
 // CountUp function for Happy Endings counter
-jQuery(document).ready(function($) {
-            $(".num").counterUp({delay:15, time:1000});
+$(function() {
+    $(".num").counterUp({delay:15, time:1000});
+});
+  
+// Dropdown-menu function        
+$(function() {
+        $(".navbar-item.has-dropdown").click(function(e) {
+            if ($(".navbar-burger").is(':visible')) {
+                $(this).toggleClass("is-active");
+            }
         });
-
-// jQuery function for form validator         
-//$(function() {
-    //$("form[name='registration']").validate({
-    // Specify validation rules
-   // rules: {
-    //  name: "required",
-     // email: {
-    //    required: true,
-        // Specify that email should be validated
-        // by the built-in "email" rule
-     //   email: true
-     // },
-     // phone: "required",
-     // subject: "required",
-      //question: "required"
-   // },
-    // Specify validation error messages
-   // messages: {
-   //   name: "Please enter your name",
-   //   email: "Please enter a valid email address",
-   //   phone: "Please enter a valid phone nummber",
-   //   subjet: "Please enter your subject for contacting us",
-   //   question: "Please enter your question"
-  //  },
-    // Make sure the form is submitted to the destination defined
-    // in the "action" attribute of the form when valid
-   // submitHandler: function(form) {
-  //    form.submit();
-  //  }
- // });
-//});
+        $(".navbar-item > .navbar-link").click(function(e) {
+            if ($(".navbar-burger").is(':visible')) {
+                e.preventDefault();
+            }
+        });
+        $(window).resize(function(e) {
+            if (!$(".navbar-burger").is(':visible') && $(".navbar-item.has-dropdown.is-active").length) {
+                $(".navbar-item.has-dropdown.is-active").removeClass('is-active');
+            }
+        });
+});
