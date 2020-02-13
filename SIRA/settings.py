4d20febeb1b5ab315e52d5369ab7b06c5ad77966
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2020-02-13 00:20:32 +0000

    Added django-widget tweaks to installed apps

--- SIRA/settings.py
+++ SIRA/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'widget_tweaks',
     'home_page',
     'about',
     'get_involved',
