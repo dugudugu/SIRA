1820dae009ddf3ff39c59fb89b77246cc22f164a
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2020-02-14 18:00:24 +0000

    Styled all the forms for the accounts app using django-widget-tweaks

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,15 +4,8 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'input', 'placeholder': 'Email'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Password'}))
-    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Repeat password'}))
+    email = forms.EmailField()
     
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2',]
-        widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'input', 'placeholder': 'First name' ,'autofocus': True, 'required': True}),
-            'last_name': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Last name' ,'autofocus': True, 'required': True}),
-            'username': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Username' ,  'required': True}),
-        }
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2',]
