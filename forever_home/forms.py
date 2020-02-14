dbd7b4a3bfb5e58ec339d39af15d50d5ea0d7b68
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2020-02-14 23:41:36 +0000

    Added function to model, for the search function

--- forever_home/forms.py
+++ forever_home/forms.py
@@ -5,8 +5,9 @@ from .models import HappyEnding
 class HappyEndingForm(forms.ModelForm):
     class Meta:
         model = HappyEnding
-        fields = ['name', 'adoption_date', 'adoption_image',]
+        fields = ['name', 'status', 'adoption_date', 'adoption_image',]
         widgets =  {
             'name' : forms.TextInput(attrs={'class': 'input', 'placeholder': 'e.g. Buddy', 'required': True}),
+            'status' : forms.NullBooleanSelect(attrs={'class': 'input' }),
             'adoption_date' : forms.TextInput(attrs={'class': 'input', 'placeholder': 'Day of the adoption e.g. 20 January 2020', 'required': True}),
         }           
--- forever_home/models.py
+++ forever_home/models.py
@@ -1,12 +1,26 @@
 from django.db import models
+from django.db.models import Q
 from django.urls import reverse
+
+
+class DogSearchManager(models.Manager):
+    def search(self, query=None):
+        qs = self.get_queryset()
+        if query is not None:
+            or_lookup = (Q(name__icontains=query)
+                        )
+            qs = qs.filter(or_lookup).distinct() # distinct() is often necessary with Q lookups
+        return qs
 
 
 # Model for dogs who have been adopted
 class HappyEnding(models.Model):
     name = models.CharField(max_length=100, default='')
+    status = models.BooleanField(verbose_name='For adoption', default='False')
     adoption_date = models.CharField(max_length=20, default='')
     adoption_image = models.ImageField(upload_to='adoption_images/', blank=True)
+    
+    objects = DogSearchManager()
     
     def __str__(self):
         return self.name
