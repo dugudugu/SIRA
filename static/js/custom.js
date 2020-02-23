94bcdf5b3891f958f3a25cb271476be28c9bacba
Ubuntu <ubuntu@ip-172-31-47-112.eu-central-1.compute.internal>
2020-02-23 21:46:48 +0000

    Added lazy image loading functinality to home page

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,28 @@
+// Lazy loading images credits to Jeff Delaney
+/* global document */
+
+const targets = document.querySelectorAll('img');
+
+const lazyLoad = target => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute('data-lazy');
+
+        img.setAttribute('src', src);
+        img.classList.add('fade');
+
+        observer.disconnect();
+      }
+    });
+  });
+  io.observe(target)
+};
+targets.forEach(lazyLoad);
+
+
 // LightBox function credits to Marcos Oliveira
 /* global document */
 
